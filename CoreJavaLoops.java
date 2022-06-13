7b2fa6a0ab92551803df9a6a2722f5e9b4632778
michaellowe94 <103666433+michaellowe94@users.noreply.github.com>
2022-06-13 14:03:24 -0700

    Add files via upload

---
+++ CoreJavaLoops.java
@@ -0,0 +1,102 @@
+
+public class CoreJavaLoops {
+
+	public static void main(String[] args) {
+		
+//1
+//		for (int a = 0; a <= 10; a++) {
+//			System.out.println(a);
+//		}
+		
+//2
+//		int b = 0;
+//		while (b <= 100) {
+//			System.out.println(b);
+//			b+= 10;
+//		}
+		
+//3
+//		int c = 0;
+//		do {
+//			System.out.println(c);
+//			c++;
+//		}
+//		while (c <= 10);
+		
+//4
+//		for (int d = 5; d < 100; d++) {
+//			if (d >= 25 && d <= 75)
+//				continue;
+//			if ( d%5 == 0)
+//				System.out.println(d);
+//			}
+		
+// 5
+//		for (int e = 0; e <= 100; e++) {
+//			if (e > 50)
+//				break;
+//			if (e % 5 == 0)
+//				System.out.println(e);
+//		}
+		
+// 6
+//		int weeks = 2;
+//		int days = 5;
+//		
+//		for (int w = 1; w <= 2; w++) {
+//			System.out.println("Week " + w);
+//		for (int d = 1; d <= 5; d++) {
+//			System.out.println("Day " + d);
+//		}
+//		}
+		
+// 7
+//		 int palindrome;
+//	        for(int p=10; p<=200; p++){
+//	            int num = p;
+//	            palindrome = 0;
+//	            while(num > 0){
+//	               palindrome =  palindrome * 10 + num % 10;
+//	               num = num / 10;
+//	          } if(p == palindrome)
+//	            System.out.println(p);
+//	        }
+	        
+// 8
+//		int num1 = 0;
+//		int num2 = 1;
+//		for (int i = 0; i<=50; i++) {
+//			System.out.println(num1 + " ");
+//			int sum1 = num1 + num2;
+//			num1 = num2;
+//			num2 = sum1;
+//		}
+//		int count = 50, num1 = 0, num2 = 1;
+//		for (int i = 1; i <= count; ++i)
+//        {
+//			if (num1 >= 50) {
+//				break;
+//			}
+//            System.out.print(num1+" ");
+//            int sumOfPrevTwo = num1 + num2;
+//            num1 = num2;
+//            num2 = sumOfPrevTwo;
+//        }
+		
+// 9
+//		for (int i = 0; i < 10; i++) {
+//			for (int j = 0; j < 10; j++) {
+//				System.out.println("Inner loop: i: " + i + ", j: " + j);
+//			}
+//		}
+}
+}
+
+	
+	
+	
+	
+	
+	
+
+
